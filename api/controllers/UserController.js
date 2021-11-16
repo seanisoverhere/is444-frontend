c979d0db6fa4a3de44ab2aa6a1eb36d57c089942
Lance F <daifa.fu.2019@scis.smu.edu.sg>
2021-11-16 22:54:54 +0800

    completed login

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,21 +6,52 @@ const tBankURL = process.env.TBANK_URL
 const tBankHeaders = require('../services/TBankHeaderService');
 
 class UserController {
-    static async login(userID, pin, otp, callback = (status, payload) => {})) {
+    static async login(userID, pin, otp, callback = (status, payload) => { }) {
         try {
-            if (!otp){
-                callback(418, {
-                    message: 'OTP is required'
-                });
-            } else if (!userID || !pin){
+            if (!userID || !pin) {
                 callback(400, {
                     message: 'UserID and PIN are required'
                 });
+            } else if (!otp) {
+                const { header } = tBankHeaders.requestOTP(userID, pin);
+                const reqURL = `${ tBankURL }?Header=${header}`;
+                axios
+                    .get(reqURL)
+                    .then(response => {
+                        const { data } = response;
+                        const globalErrorID = data.Content.ServiceResponse.ServiceRespHeader.GlobalErrorID;
+                        
+                        if (globalErrorID === "010000") {
+                            callback(418, {
+                                message: 'OTP is required'
+                            });
+                        } else if (globalErrorID === "010010") {
+                            callback(401, {
+                                message: 'Invalid Login Details'
+                            });
+                        }
+                    });
+                
             } else {
-                const header = tBankHeaders.loginCustomer(userID, pin, otp);
-                
-                const reqURL = `${tBankURL}/api/v1/customer/accounts`;
-                
+                const { header } = tBankHeaders.loginCustomer(userID, pin, otp);
+                const reqURL = `${ tBankURL }?Header=${header}`;
+                axios
+                    .get(reqURL)
+                    .then(response => {
+                        const { data } = response;
+                        const globalErrorID = data.Content.ServiceResponse.ServiceRespHeader.GlobalErrorID;
+                        
+                        if (globalErrorID === "010000") {
+                            callback(200, {
+                                message: 'Login Successful'
+                            });
+                        } else if (globalErrorID === "010010") {
+                            callback(401, {
+                                message: 'Invalid OTP'
+                            });
+                        }
+                    });
+
             }
         } catch (error) {
             callback(500, {
@@ -28,13 +59,13 @@ class UserController {
             });
         }
     }
-    
-    
+
+
     // static async login(username, password, callback = (status, payload) => {}) {
     //     const validationError = []
     //     username ? null : validationError.push('username cannot be empty')
     //     password ? null : validationError.push('password cannot be empty')
-        
+
     //     if (validationError.length == 0) {
     //         try {
     //             const user = await prisma.user.findUnique({
@@ -42,7 +73,7 @@ class UserController {
     //                     username: username,
     //                 }
     //             });
-                
+
     //             if (user) {
     //                 const isMatch = AuthService.verifyHash(password, user.pHash);
     //                 if (isMatch) {
@@ -60,28 +91,28 @@ class UserController {
     //                     message: 'User not found'
     //                 })
     //             }
-                
+
     //         } catch (error) {
     //             console.error(error)
     //             callback(500, {
     //                 "error": error.message  
     //             })
     //         }
-            
+
     //     } else {
     //         callback(400, {
     //             "error": validationError
     //         })
     //     }
     // }
-    
+
     // static async register(username, password, callback = (status, payload) => {}) {
     //     // Add more user details later. Pass over by JSON object
-        
+
     //     const validationError = []
     //     username ? null : validationError.push('username cannot be empty')
     //     password ? null : validationError.push('password cannot be empty')
-        
+
     //     if (validationError.length == 0) {
     //         try {
     //             const user = await prisma.user.create({
@@ -90,7 +121,7 @@ class UserController {
     //                     pHash: AuthService.generateHash(password)
     //                 }
     //             });
-                
+
     //             callback(201, {
     //                 message: 'User created',
     //                 user: user
@@ -101,7 +132,7 @@ class UserController {
     //                 "error": error.message  
     //             })
     //         }
-            
+
     //     } else {
     //         callback(400, {
     //             "error": validationError
